5208ac5beb13b199740b2b1622a8c18a8cb66ff5
G Kalyan Raju <kalyan2642@gmail.com>
2020-06-28 15:44:34 +0530

    Update couple.js

--- couple.js
+++ couple.js
@@ -21,7 +21,7 @@ app.listen(80, function () {
 })*/
 var WebSocket = require('ws');
 var WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({app})
+  wss = new WebSocketServer({server:app})
   var userCount = [];
 
   wss.broadcast = function broadcast(data) {
